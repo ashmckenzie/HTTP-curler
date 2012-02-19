711e7cc2a880781117dba0e89a68dde7708a04dc
Ash McKenzie <ash@greenworm.com.au>
2012-02-19 22:19:13 +1100

    Initial Commit

---
+++ HTTP curler/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  HTTP curler
+#
+#  Created by Ashley McKenzie on 19/02/12.
+#  Copyright 2012 __MyCompanyName__. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
---
+++ HTTP curler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HTTP curler
+//
+//  Created by Ashley McKenzie on 19/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
