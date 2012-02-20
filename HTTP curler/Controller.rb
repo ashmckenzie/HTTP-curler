3f0963807d3b81e6b51dfcf0f664bb0999defd5e
Ash McKenzie <ash.mckenzie@playup.com>
2012-02-20 17:04:04 +1100

    works!

--- HTTP curler/Controller.rb
+++ HTTP curler/Controller.rb
@@ -4,30 +4,35 @@ require 'net/http'
 class Controller
   attr_accessor :url
   attr_accessor :output
-  attr_accessor :headers
+  attr_accessor :api_version
+  
+  API_V1_HEADERS = "Accept: application/vnd.playup.sport ;"
   
   def awakeFromNib
   end  
   
-  def hitUrl(sender)
-    output.string = ""
-    
+  def hitUrl(sender)   
     uri = URI.parse(url.stringValue)
     request = Net::HTTP::Get.new(uri.path)    
+
+    headers = ""
     
-    if headers
-     headers.stringValue.split(/;/).each do |header|
-        key, value = header.split(/:/)
-        key.strip!
-        value.strip!
-        request.add_field(key, value)
-      end
+    if api_version.selectedCell.tag == 1
+      headers = API_V1_HEADERS
+    end
+    
+    headers.split(/;/).each do |header|
+      key, value = header.split(/:/)
+      key.strip!
+      value.strip!
+      request.add_field(key, value)
     end
       
     response = Net::HTTP.new(uri.host).start do |http|
       http.request(request)
     end
                                 
-    output.string = response.body                                
+    output.string = response.body
+    output.textStorage.font = NSFont.fontWithName("Andale Mono", size:12.0)
   end
 end
